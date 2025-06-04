7dde3c9c07c02bb9ed877d95be0d5271541da4e3
luthfilaude <muhamadupi54@gmail.com>
2025-06-04 18:53:16 +0700

    Update package dependencies and refine navbar component structure

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -12,7 +12,7 @@
                 </a>
             </div>
 
-    {{-- <div class="md:flex md:items-center md:gap-12">
+            {{-- <div class="md:flex md:items-center md:gap-12">
         <nav aria-label="Global" class="hidden md:block">
           <ul class="flex items-center gap-6 text-sm">
             <li>
@@ -104,7 +104,7 @@
                     </ul>
                 </nav>
                 <!-- Mobile Menu -->
-                <div :class="{ 'block': open, 'hidden': !open }" x-show="open" @click.away="open = false"
+                {{-- <div :class="{ 'block': open, 'hidden': !open }" x-show="open" @click.away="open = false"
                     @keydown.escape.window="open = false"
                     class="absolute inset-x-0 top-full z-50 bg-white shadow-lg border-t border-gray-200 transition-all md:hidden">
                     <nav aria-label="Mobile" class="p-4">
@@ -135,26 +135,135 @@
                             </li>
                         </ul>
                     </nav>
+                </div> --}}
+                <div :class="{ 'block': open, 'hidden': !open }" x-show="open" @click.away="open = false"
+                    @keydown.escape.window="open = false"
+                    class="absolute inset-x-0 top-full z-50 bg-white shadow-lg border-t border-gray-200 transition-all md:hidden">
+                    <nav aria-label="Mobile" class="p-4">
+                        <ul class="space-y-1">
+                            <li>
+                                <a href="#"
+                                    class="flex items-center gap-2 rounded-lg bg-gray-100 px-4 py-2 text-gray-700">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-5 opacity-75 shadow-sm">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z" />
+                                    </svg>
+
+
+                                    <span class="text-sm font-medium"> About </span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="#"
+                                    class="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-5 opacity-75 shadow-sm">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+                                    </svg>
+
+
+                                    <span class="text-sm font-medium"> Careers </span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="#"
+                                    class="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-5 opacity-75 shadow-sm">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122" />
+                                    </svg>
+
+
+                                    <span class="text-sm font-medium"> History </span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="#"
+                                    class="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672Zm-7.518-.267A8.25 8.25 0 1 1 20.25 10.5M8.288 14.212A5.25 5.25 0 1 1 17.25 10.5" />
+                                    </svg>
+
+
+                                    <span class="text-sm font-medium"> Services </span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="#"
+                                    class="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+                                    {{-- <svg xmlns="http://www.w3.org/2000/svg" class="size-5 opacity-75 shadow-sm"
+                                        fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                                    </svg> --}}
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-5 opacity-75 shadow-sm">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+                                    </svg>
+
+
+                                    <span class="text-sm font-medium"> Projects </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#"
+                                    class="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-5 opacity-75 shadow-sm">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M12.75 19.5v-.75a7.5 7.5 0 0 0-7.5-7.5H4.5m0-6.75h.75c7.87 0 14.25 6.38 14.25 14.25v.75M6 18.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                                    </svg>
+
+
+                                    <span class="text-sm font-medium"> Blog </span>
+                                </a>
+                            </li>
+                            <li class="mt-4 sm:hidden">
+                                <hr class="border-gray-200" />
+                                <div class="mt-4 flex items-center justify-center gap-4">
+                                    <a class="inline-block rounded-sm border border-teal-600 bg-teal-600 px-8 py-2 text-sm font-medium text-white hover:bg-transparent hover:text-teal-600 focus:ring-3 focus:outline-hidden"
+                                        href="#">
+                                        Login
+                                    </a>
+                                    <a href="#"
+                                        class="inline-block rounded-sm border border-teal-600 px-8 py-2 text-sm font-medium text-teal-600 hover:bg-teal-600 hover:text-white focus:ring-3 focus:outline-hidden">Register</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
 
                 <!-- Action buttons + hamburger -->
                 <div class="flex items-center gap-4">
                     <div class="sm:flex sm:gap-4">
-                        <a class="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow-sm"
-                            href="#">
-                            Login
-                        </a>
+                        <div class="hidden sm:flex">
+                            <a class="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow-sm"
+                                href="#">
+                                Login
+                            </a>
+                        </div>
 
                         <div class="hidden sm:flex">
-                            <button class="btn btn-outline btn-accent rounded-md px-5 py-2.5 text-sm font-medium">Register</button>
+                            <button
+                                class="btn btn-outline btn-accent rounded-md px-5 py-2.5 text-sm font-medium">Register</button>
                         </div>
                     </div>
                     <!-- Hamburger Button -->
                     <div class="block md:hidden">
                         <button @click="open = !open"
                             class="rounded-sm bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="size-5" fill="none" viewBox="0 0 24 24"
-                                stroke="currentColor" stroke-width="2">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="size-5" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                             </svg>
                         </button>
