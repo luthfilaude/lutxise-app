513983f5901f9f72fdd96157ffdb764e19585926
luthfilaude <muhamadupi54@gmail.com>
2025-06-04 10:59:55 +0700

    Add navbar component and integrate Alpine.js and DaisyUI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'Portfolio')</title>
-    <link rel="stylesheet" href="@vite('resources/css/app.css')">
+    <title>@yield('title')</title>
+    @vite('resources/css/app.css')
 </head>
+
 <body>
     @include('components.navbar')
-    <main>
-    @yield('content')
+    <main class="pt-16">
+        @yield('content')
     </main>
     @include('components.footer')
-    <script src="@vite('resources/js/app.js')"></script>
+    @vite('resources/js/app.js')
+    @yield('scripts')
 </body>
+
 </html>
